02ff5ff28aada77ab4a8fd872ecb0ec9c5982fa6
dejotaerre <dejotaerre@googlemail.com>
2023-10-22 22:30:49 -0300

    build

--- ensambla.bat
+++ ensambla.bat
@@ -1,8 +1,9 @@
 @echo off
+echo.
 
-del *.rom
+del *.rom > nul
 
-./bin/sjasmplus.exe --lstlab --lst=plus3.lst plus3.asm
+.\bin\sjasmplus.exe --lstlab --lst=plus3.lst plus3.asm
 
 if errorlevel 1 (
 	echo.
@@ -13,17 +14,23 @@ if errorlevel 1 (
 	exit 1
 )
 
-REM ROMs de 64K por cada chip para quemar en EEPROMS DOBLE ROM en sócalo 1 y 2
+REM ROMs de 32K por cada chip para quemar en EPROMS en sócalos 1 y 2
 REM con la versión en inglés de las ROMs +3 v4.1
-copy /b roms_standar_p3\P3-41A.ROM + p3t_rom0.rom + p3t_rom1.rom p3t_rom_a.rom
-copy /b roms_standar_p3\P3-41B.ROM + p3t_rom2.rom + p3t_rom3.rom p3t_rom_b.rom
+copy /b p3t_rom0.rom + p3t_rom1.rom 32K_ROM-A.rom
+copy /b p3t_rom2.rom + p3t_rom3.rom 32K_ROM-B.rom
 
-if exist ../p3e-file-selector (
+REM ROMs de 64K por cada chip para quemar en EPROMS "DOBLE" ROM en sócalos 1 y 2
+REM con la versión en inglés de las ROMs +3 v4.1
+copy /b roms_standar_p3\P3-41A.ROM + p3t_rom0.rom + p3t_rom1.rom 64K_ROM-A.rom
+copy /b roms_standar_p3\P3-41B.ROM + p3t_rom2.rom + p3t_rom3.rom 64K_ROM-B.rom
+
+if exist ..\p3e-file-selector (
 	copy /Y p3t_rom?.rom ..\p3e-file-selector
 )
 
 cd bin
 
+REM genero los comandos para crear una imagen de disquete para testeos en el emulador
 echo new -f PCW3 ..\test.dsk >  makedsk
 echo open -f PCW3 ..\test.dsk >> makedsk
 echo mput -f -b taps\*.tap >> makedsk
@@ -46,7 +53,7 @@ set fuse_opciones=%fuse_opciones% --rom-plus3-3 ..\p3t_rom3.rom
 set fuse_opciones=%fuse_opciones% --simpleide-masterfile +3e8bits.hdf
 set fuse_opciones=%fuse_opciones% --graphics-filter tv4x
 set fuse_opciones=%fuse_opciones% --pal-tv2x
-rem set fuse_opciones=%fuse_opciones% --rom-multiface3 ..\fusew\mf3.rom
+set fuse_opciones=%fuse_opciones% --rom-multiface3 ..\fusew\roms\mf3.rom
 set fuse_opciones=%fuse_opciones% --drive-plus3a-type "Double-sided 80 track"
 set fuse_opciones=%fuse_opciones% --drive-plus3b-type "Double-sided 80 track"
 
@@ -64,7 +71,7 @@ cd ..
 %fuse_exec% %fuse_opciones%
 
 rem LIMPIEZA DE ARCHIVOS NO NECESARIOS LUEGO DEL TESTING
-rem del *.lst
-rem del *.dsk
+del *.lst
+del *.dsk
 
 exit 0
